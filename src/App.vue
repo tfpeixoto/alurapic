ea589a7fd43089fa96791e337091dfc067dec77b
˜Thiago <thiago.peixoto@hotmart.com>
2021-12-01 00:40:11 -0300

    Primeiro commit do curso de Vue.js

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.index">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from "./components/shared/painel/Painel.vue";
+
+export default {
+  components: {
+    'meu-painel': Painel
+  },
+
+  data() {
+    return {
+      titulo: "Alurapic",
+      fotos: []
+    };
+  },
+
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fotos => (this.fotos = fotos));
+  }
+};
+</script>
+
+<style>
+.corpo {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+</style>
